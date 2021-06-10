b9f936b5a91c8307ae3e795b4573e242123b8bc7
Ellen Studer <ellenmstuder@gmail.com>
2021-06-09 22:26:27 -0500

    removing duplicative cors configuration

--- index.js
+++ index.js
@@ -33,21 +33,6 @@ let auth = require('./auth')(app);
 
 //------------------------------------------------------------------------------
 
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       let message = 'The CORS policy for this application doesn`t allow access from origin ' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
-//------------------------------------------------------------------------------
-
 // Landing welcome message
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix!')
