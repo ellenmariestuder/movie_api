f9fe8c42d5a07fa719099c28af52933ec374ca70
Ellen Studer <ellenmstuder@gmail.com>
2021-06-09 20:01:55 -0500

    temporarily disable duplicative CORS configuration

--- index.js
+++ index.js
@@ -33,18 +33,18 @@ let auth = require('./auth')(app);
 
 //------------------------------------------------------------------------------
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com'];
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application doesn`t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       let message = 'The CORS policy for this application doesn`t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 //------------------------------------------------------------------------------
 
